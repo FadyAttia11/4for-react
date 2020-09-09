ea49664b9881f4120a510fced191f4edc3525404
FadyAttia <fadyattia11@gmail.com>
2020-09-09 13:40:34 +0200

    Updates

---
+++ precache-manifest.4e7b84ca43e34795519f4ebed6a4185f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea519596745f8d7559bf1de5b1855c6b",
+    "url": "/4for-react/index.html"
+  },
+  {
+    "revision": "5329a7449ea375b0bcaf",
+    "url": "/4for-react/static/css/main.00917c4b.chunk.css"
+  },
+  {
+    "revision": "2c79f6144854aa28562b",
+    "url": "/4for-react/static/js/2.3831924e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/4for-react/static/js/2.3831924e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5329a7449ea375b0bcaf",
+    "url": "/4for-react/static/js/main.8f48fc2d.chunk.js"
+  },
+  {
+    "revision": "13f59c90497fa0055ff4",
+    "url": "/4for-react/static/js/runtime-main.b91761e8.js"
+  },
+  {
+    "revision": "3cd2fabe4a2605bc511baabc28e1ca6e",
+    "url": "/4for-react/static/media/4jobs.3cd2fabe.jpg"
+  },
+  {
+    "revision": "f80ded8d106bcffc5d52a5f67185df13",
+    "url": "/4for-react/static/media/4movies.f80ded8d.jpg"
+  },
+  {
+    "revision": "358a0b0ba8a7b282f5861b8a31894b32",
+    "url": "/4for-react/static/media/4places.358a0b0b.jpg"
+  },
+  {
+    "revision": "dedeca960c84cf1926e47998c270c2f8",
+    "url": "/4for-react/static/media/4shopping.dedeca96.jpg"
+  },
+  {
+    "revision": "eab2612351903aa63810ca938958ddff",
+    "url": "/4for-react/static/media/hero-bg2.eab26123.jpg"
+  },
+  {
+    "revision": "f636c6b7dbf5ef324a35971e13d7776a",
+    "url": "/4for-react/static/media/img-1.f636c6b7.png"
+  },
+  {
+    "revision": "6150f21d4ed398550cebe6e3f5b39cbe",
+    "url": "/4for-react/static/media/personal-img.6150f21d.jpg"
+  }
+]);
